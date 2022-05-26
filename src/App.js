e9b4ec2cdedb6924acfd17b5152eacd4c197ef5a
AkashAbrrar <mabrrar9@gmail.com>
2022-05-26 22:52:33 +0600

    this commit for page not found routes setup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Home from './Pages/Home/Home';
 import Footer from './Pages/Shared/Footer';
 import Login from './Pages/Shared/Login';
+import LostPage from './Pages/Shared/LostPage';
 import Navbar from './Pages/Shared/Navbar';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='*' element={<LostPage></LostPage>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
---
+++ src/Pages/Shared/LostPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const LostPage = () => {
+    return (
+        <div>
+            <div>
+                <h2 className='text-4xl text-center font-bold text-red-500'>The Page You Looking For Is Not Found</h2>
+            </div>
+            <div>
+                <img className='h-3/4 w-72' src="https://afftray.com/wp-content/uploads/2021/08/404-1.png" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default LostPage;
